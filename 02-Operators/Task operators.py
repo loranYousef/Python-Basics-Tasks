5518d53f58debe5b59dd9a98141d00106d820e32
loranYousef <loganyousef@gmail.com>
2023-02-27 14:43:49 +0100

    solvin operator task questions

---
+++ 02-Operators/Task operators.py
@@ -0,0 +1,79 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # 1. What is differneces between = , == 
+...  # (=) to define a variable value x = 2
+...  x = 2
+...  # (==) its mean is x equal 2 ?
+...  x== 2
+... True
+... 
+... 
+... # 2.What do we mean by using !=
+...  # its mean its not equals
+...  x != 2
+... False
+... 
+... 
+... # 3.What is operator precedence
+...  # its mean wish operators comes first 
+... 
+... 
+... # 4. Creat a variable x with value 10
+...  x = 10
+... 
+... 
+... # 5.increase x Value by 15 using Assignment operators 
+...  x= x + 5
+...  x
+... 15
+... 
+... 
+... # 6.Divide the x Value by 5 using assignment operators 
+...  x /= 5
+...  x
+... 3.0
+... 
+... 
+... # 7.Multiply the x value by 10 using assignment operators
+... >>> x *= 10
+... >>> x
+30.0
+
+
+# 8.Get module of x on 3 using assignment operators 
+ x %=3
+ x
+0.0
+
+
+# 9.Using Python print the module of 11 to 4
+ x = 11
+ x %=4
+ print (x)
+3
+
+
+# 10. Print the exponent of 2,3
+ x = 2
+ x **=3
+ x
+8
+
+
+# 11.Divide 11 by 3 using Python division 
+ x = 11
+ x /=3
+ x
+3.6666666666666665
+
+
+# 12.can we divide 11 by 3 and get an integer numbers?
+
+ x = 11
+ x //=3
+ x
+3
+
+
+
+
