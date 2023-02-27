5a02294730244caef68a518c71e8ecd7fad108ca
loranYousef <loganyousef@gmail.com>
2023-02-27 12:52:09 +0100

    solving Variable Task Questions

---
+++ 01-Variables/Variables Task.py
@@ -0,0 +1,60 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # 1. Create a Boolean variable named x
+>>> x = True
+>>> # 2. Create an integer variable named y
+>>> y = 1
+>>> # 3. Create a float variable named z
+>>> z = 2.5
+>>> # 4. Create a string variable named s
+>>> s = 'ahmad'
+
+>>> # 5. Convert the int variable to float
+>>> x = 3
+>>> float(x)
+3.0
+
+>>> # 6. Can we convert the str to int?
+>>> y = '5'
+>>> int(y)
+5
+>>> # 7. Create a List of Numbers from 1 to 5
+>>> x = [1,2,3,4,5]
+>>> list(range(1,6))
+... [1, 2, 3, 4, 5]
+... 
+... 
+... >>> # 8. Create a tuple from 10 to 15
+... >>> x = (10,11,12,13,14,15)# that is first way. 2. we can make the folowing steps also
+... 
+... >>> tuple(range(10,16))
+... (10, 11, 12, 13, 14, 15)
+... 
+... >>> # 9. convert list to tuple
+... 
+... >>> x = [1,2,3,4]
+... >>> list[x]
+... list[[1, 2, 3, 4]]
+... >>> tuple (x)
+... (1, 2, 3, 4)
+... 
+... >>> # 10. Create a dict of 3 values
+... >>> x = {'ahamd': 20, 'imad':30, 'hamoud':40}
+... 
+... 
+... >>> # 11. Can we use semi colon ; with python ?
+... # Yes
+... 
+... >>> # 12. Python is interpreted or compiled ?
+... >>> # python it is interpreted.
+... 
+... 
+... >>> # 12. Python is interpreted or compiled ?
+... >>> # python it is interpreted.
+... 
+... >>> # 13. What is defferences between low level & high level ?
+... >>> # Low level its hard to write the Code for Programer but its fast to run
+... >>> # high level its easy to writing the Code but its slower to run . the differnce is the Understanding the Code between Programer and PC
+... 
+... 
+... 
