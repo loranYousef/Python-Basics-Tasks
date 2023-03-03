6dbfa7a9bb9fcf895f457151bde62602f62e2024
loranYousef <loganyousef@gmail.com>
2023-03-03 11:06:55 +0100

    Loop task

---
+++ 05-Loop Task/loop Task.py
@@ -0,0 +1,35 @@
+#1. Print numbers from 0 to 10 using while
+
+'''
+x=0
+while x < 11 :
+    print(x)
+    x +=1
+'''
+    
+#2. Print numbers from 0 to 1o using for
+'''
+for x in range(0,11):
+    print(x)
+
+'''
+
+#3. Stop the loop if the number = 5
+'''
+
+for x in range(0,11):
+    if x == 5 :
+        break
+    print(x)
+'''
+#4. Skip the 5 iteration from print
+'''
+
+for x in range(0,11):
+    if x == 5 :
+        continue
+    print(x)
+
+'''
+#5. Print multiplication table from 1 to 5
+
