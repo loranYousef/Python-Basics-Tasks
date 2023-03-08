d07f828a9acc50b554aebb6a326b4918e20878d4
loranYousef <loganyousef@gmail.com>
2023-03-08 10:16:03 +0100

    datatypes task

---
+++ 06-Datatypes/Datatypes.py
@@ -0,0 +1,23 @@
+#1. With is the differences between '' "" '''  '''
+ 
+#if we have some strin like names include (') we use double (" what's or it's ") to put all in string. the ''' Triple quotes are used to create multiline strings.
+#Triple quotes can also be used for docstrings, which are used to document functions, classes, and modules:
+
+#2. Create a string with the name 'mystero'
+
+'''
+a = 'mystro'
+
+'''
+
+
+#3. Make the string letters capital
+'''
+a = 'mystro'
+a.upper()
+
+'''
+#4. Create a list of values from 10 to 20
+
+
+    
