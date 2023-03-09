66256b231ef0d672c3d48f2bfba5debe497abfd3
loranYousef <loganyousef@gmail.com>
2023-03-09 08:02:56 +0100

    datatypes Task

--- 06-Datatypes/Datatypes.py
+++ 06-Datatypes/Datatypes.py
@@ -1,23 +1,79 @@
 #1. With is the differences between '' "" '''  '''
  
 #if we have some strin like names include (') we use double (" what's or it's ") to put all in string. the ''' Triple quotes are used to create multiline strings.
-#Triple quotes can also be used for docstrings, which are used to document functions, classes, and modules:
+#Triple quotes can be used for docstrings, which are used to document functions, classes, and modules:
 
 #2. Create a string with the name 'mystero'
 
-'''
+
 a = 'mystro'
 
-'''
+
 
 
 #3. Make the string letters capital
-'''
+
 a = 'mystro'
 a.upper()
 
-'''
+
 #4. Create a list of values from 10 to 20
 
 
+x = print(tuple( range(10,21)))
+
+
+
+
+list_range = []
+for x in range(10,21):
+    list_range.append(x)
     
+print(list_range)
+
+
+
+
+#5. Add 30 to the end of the list
+
+list_range.index(20)
+
+ist_range.insert(11,30)
+list_range
+
+#6. Add 40 as the second element of the list
+list_range.insert(2,40)
+
+#7. Print how many elements in the list
+len(list_range)
+
+#8. replace the second element in the list with 100
+list_range[2] = 100
+list_range
+
+#9. Create a tuple with values from 1 to 5 10. 
+
+x = print(tuple( range(1,6)))  
+
+#Can we add 10 to the end of the tuple?
+
+# no but if we changit to list , we could
+
+#11. Create a dict of value Mahmoud:28, ahmed:30
+
+d = {'Mahmoud':28 , 'ahmed':30}
+d.values()
+d.get('Mahmoud')
+
+
+#12. Print Mahmoud age from the dict
+
+d.values()
+d.get('Mahmoud')
+
+
+#13. What is the differences between mutable and immutable data types ?
+'''
+ Mutable data: can be edit or changed
+ immutable data: can not but is faster than mutable
+ '''
