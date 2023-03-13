93594bb0f04591a28194479f7bd34911be703b9e
loranYousef <loganyousef@gmail.com>
2023-03-13 17:55:45 +0100

    oop task 1

---
+++ 07-oop task/oop task 1.py
@@ -0,0 +1,94 @@
+# 1. Explain in few words what we mean by a class give and example
+class is some of few functions that we call them with class name
+EX:
+    class Bank:
+       
+    c1= Bank
+    
+
+
+#2. create a simple class names calculator
+    class Calc:
+        def __init__(self):
+    c1 = Calc
+
+#3. Create a constructor that prints Welcome message
+      class Calc:
+        def __init__(self):
+        print('welcome')
+    c1 = Calc
+
+#4. Add 2 methods to the class sum & mull
+     
+ class Calc:
+        def __init__(self):
+        print('welcome')
+        def mysum(x,y):
+            result= x+y
+            print(result)
+c1 = Calc
+c1.mysum(5,6)
+#5. The sum method return the sum of 2 arguments x and y
+ class Calc:
+        def __init__(self):
+            print('welcome')
+        def mysum(x,y):
+            result= x+y
+            return result
+c1 = Calc
+print(result)
+
+#6. The mull method return the multiplication of the arguments x and y
+ class Calc:
+        def __init__(self):
+            print('welcome')
+        def mysum(self,x,y):
+            result= x*y
+            return result
+
+#7. Take an object from the class
+class Calc:
+       def mysum(self,x,y):
+           return x+y
+            print(x+y)
+            
+
+c1 = Calc()
+c1.mysum(5,6)
+
+#8. Call the sum method with 10 , 20
+class Calc:
+       def mysum(self,x,y):
+           return x+y
+            print(x+y)
+       
+            
+
+c1 = Calc()
+c1.mysum(10,20)
+
+#9. Call the mull method with 10 , 20
+class Calc:
+     def mull(self,x,y):
+            return x*y
+            print(x*y)
+
+c1= Calc()
+c1.mull(10,20)
+
+#10. Explain in few words why we call the self in methods
+self its to defin a variable that equal the class name
+to make kontakt with class and self should always have place in functions
+
+#11. What we mean with OOP 4 Pillars
+encapsulation
+inheritance
+polymorphism
+abstraction
+
+we should use thise 4 methode when we writing code
+#12. Why we use OOP in our code
+it make code modular and faster and all programing language use oop 
+
+
+#Python OOP Tasks Part 1
