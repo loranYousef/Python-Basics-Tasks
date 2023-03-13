c8f0ced3bbdf31bd4273c07281fa0c818b9b7e6d
loranYousef <loganyousef@gmail.com>
2023-03-13 20:10:48 +0100

    oop task

---
+++ 07-oop task/oop task 3.py
@@ -0,0 +1,29 @@
+#1. What do we mean with the term :
+#   1. Base class
+
+#   2. Parent class
+
+#   3. Sub class
+
+#   4. Main class
+
+#   5. Derived class
+
+#   6. Instance
+
+#2. Explain in few words what we mean with the word polymorphism , give an example
+
+
+#3. Can we inherit from more than one class in python
+
+
+#4. If yes how multiple inheritance works
+
+
+
+
+
+
+
+
+Python OOP Tasks Part 3
