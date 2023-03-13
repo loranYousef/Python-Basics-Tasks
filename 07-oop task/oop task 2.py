261f1001d89d8b8df4d653e0383f785a03b8c997
loranYousef <loganyousef@gmail.com>
2023-03-13 18:22:55 +0100

    oop task

--- 07-oop task/oop task 2.py
+++ 07-oop task/oop task 2.py
@@ -6,9 +6,13 @@
 #1. Create a new class names SciCalc with 3 methods , sum , mull , power all of them takes 2 argument x, y
 class Scicalc:
     def sum(self,x,y):
+        pass
         
     def mull(self,x,y):
+        pass
+    
     def power(self,x,y):
+        pass
 c1 = Scicalc()
 
 #2. Sum return the sum of x and y
@@ -18,13 +22,49 @@ class Scicalc:
     def mull(self,x,y):
     def power(self,x,y):
 c1 = Scicalc()
+c1.sum(5,6)
 
 
 #3. Mull return the multiplication of x and y
 
+class Scicalc:
+    def sum(self,x,y):
+        
+    def mull(self,x,y):
+        return x*y
+    def power(self,x,y):
+        return x,y
+c1 = Scicalc()
+c1.mull(5,6)
+
+
 #4. The power return x power y
 
+class Scicalc:
+    def sum(self,x,y):
+        
+    def mull(self,x,y):
+        
+    def power(self,x,y):
+        return x,y
+c1 = Scicalc()
+c1.power(0,1)
+
+
 #5. Take an object from the class and call the 3 methodswith any numbers
+
+class Scicalc:
+    def sum(self,x,y):
+        return x+y
+    def mull(self,x,y):
+        return x*y
+    def power(self,x,y):
+        return x,y
+c1 = Scicalc()
+c1.sum(5,6)
+c1.mull(5,6)
+c1.power(0,1)
+
 
 #6. Can we inherit from the class we created in the firsttask Calc
 
